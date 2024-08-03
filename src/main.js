8f6eba5cad8852f67e343f0465f80a17042e0e2d
Nastya-20 <62868214+Nastya-20@users.noreply.github.com>
2024-08-03 19:42:25 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// header
+/// header
 const modalOverlay = document.querySelector('.modal-overlay');
 const burgerOpen = document.querySelector('.open-burger');
 const btnClose = document.querySelector('.close-btn');
@@ -16,8 +16,7 @@ export function openModalWindow() {
 export function openMenu() {
     if (document.querySelector('.is-open-header') ) {
         modalMenu.classList.remove('is-open-header');
-    }
-    else {
+    } else {
         modalMenu.classList.add('is-open-header');
     }
 }
@@ -32,8 +31,7 @@ export function NavvMenu(evt) {
     evt.preventDefault();
     if (evt.target !== evt.currentTarget) {
         return;
-    }
-    else {
+    } else {
         modalOverlay.classList.remove('is-open-header');
         burgerOpen.style.opacity = '';
         modalClsBody.style.overflow = '';
@@ -48,21 +46,21 @@ export function linkToFooter(evt) {
     burgerOpen.style.opacity = '';
     modalClsBody.style.overflow = '';
 
-setTimeout(() => {
-    document.querySelector('#worktogether').scrollIntoView({behavior: 'smooth'});
-}, 100);
+    setTimeout(() => {
+        document.querySelector('#worktogether').scrollIntoView({ behavior: 'smooth' });
+    }, 100);
 }
 
 export function NavMenu(evt) {
     if (evt.target.tagName === 'A') {
         modalOverlay.classList.remove('is-open-header');
-        burgerOpen.style.opacity ='';
+        burgerOpen.style.opacity = '';
         modalClsBody.style.overflow = '';
 
         const targetId = evt.target.getAttribute('href');
 
         setTimeout(() => {
-            document.querySelector(targetId).scrollIntoView({behavior: 'smooth'});
+            document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
         }, 100);
     }
 }
@@ -70,231 +68,227 @@ export function NavMenu(evt) {
 //aboutme
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-  
+
 export const aboutSwiper = () => {
-  const swiper = new Swiper('.skills-conteiner', {
-      navigation: {
-      nextEl: '.my-next',
-    },
-    loop: true,
-    speed: 400,
-    mousewheel: true,
-    clickable: true,
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 0,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 0,
-      },
-      1440: {
-        slidesPerView: 6,
-        spaceBetween: 0,
-      },
-    },
-  });
+    const swiper = new Swiper('.skills-conteiner', {
+        navigation: {
+            nextEl: '.my-next',
+        },
+        loop: true,
+        speed: 400,
+        mousewheel: true,
+        clickable: true,
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 0,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 0,
+            },
+            1440: {
+                slidesPerView: 6,
+                spaceBetween: 0,
+            },
+        },
+    });
 };
 
 export function aboutme() {
-  const accordion = document.querySelector('.acc-container')
-  const accordions = Array.from(document.querySelectorAll('.acc-container'));
-  new Accordion(accordions, {
-    duration: 600,
-    showMultiple: true,
-    onOpen: function (currentElement) {
-    },
-    openOnInit: [0]
-  });
+    const accordionContainers = Array.from(document.querySelectorAll('.acc-container'));
+    new Accordion(accordionContainers, {
+        duration: 600,
+        showMultiple: true,
+        onOpen: function (currentElement) {
+        },
+        openOnInit: [0]
+    });
 }
-//projects
 
+//projects
 export const swiperProjects = () => {
-  const swiper = new Swiper('.projects-js', {
-  slidesPerView: 1,
-  spaceBetween: 0,
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-    navigation: {
-    nextEl: '.project-button-next',
-    prevEl: '.project-button-prev',
-  },
-  on: {
-      reachEnd: () => {
-        document.querySelector('.project-button-next').classList.add('disabled');
-      },
-      reachBeginning: () => {
-        document.querySelector('.project-button-prev').classList.add('disabled');
-      },
-      fromEdge: () => {
-        document.querySelector('.project-button-prev').classList.remove('disabled');
-        document.querySelector('.project-button-next').classList.remove('disabled');
-      },
-    },
-});
+    const swiper = new Swiper('.projects-js', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+        navigation: {
+            nextEl: '.project-button-next',
+            prevEl: '.project-button-prev',
+        },
+        on: {
+            reachEnd: () => {
+                document.querySelector('.project-button-next').classList.add('disabled');
+            },
+            reachBeginning: () => {
+                document.querySelector('.project-button-prev').classList.add('disabled');
+            },
+            fromEdge: () => {
+                document.querySelector('.project-button-prev').classList.remove('disabled');
+                document.querySelector('.project-button-next').classList.remove('disabled');
+            },
+        },
+    });
 }
 
 //faq
 export function faq() {
-  const accordion = document.querySelector('.faq-accordion-container')
-
-  const accordions = Array.from(document.querySelectorAll('.faq-accordion-container'));
-  new Accordion(accordions, {
-    duration: 600,
-    showMultiple: true,
-    onOpen: function (currentElement) {
-
-    },
-    //openOnInit: [0]
-  });
+    const faqAccordionContainers = Array.from(document.querySelectorAll('.faq-accordion-container'));
+    new Accordion(faqAccordionContainers, {
+        duration: 600,
+        showMultiple: true,
+        onOpen: function (currentElement) {
+        },
+    });
 }
+
 //covers
 export function setupCoverAnimation() {
-  document.addEventListener('DOMContentLoaded', () => {
-    const coversSection = document.querySelector('.cover-section');
+    document.addEventListener('DOMContentLoaded', () => {
+        const coversSection = document.querySelector('.cover-section');
 
-    const options = {
-      rootMargin: '0px',
-      threshold: 0,
-    };
+        const options = {
+            rootMargin: '0px',
+            threshold: 0,
+        };
 
-    const observerCallback = (entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          coversSection.classList.add('animate');
-          //console.log('Animation added');
-        } else {
-          coversSection.classList.remove('animate');
-          //console.log('Animation removed');
+        const observerCallback = (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    coversSection.classList.add('animate');
+                } else {
+                    coversSection.classList.remove('animate');
+                }
+            });
+        };
+
+        const observer = new IntersectionObserver(observerCallback, options);
+
+        if (coversSection) {
+            observer.observe(coversSection);
         }
-      });
-    };
-
-    const observer = new IntersectionObserver(observerCallback, options);
-
-    if (coversSection) {
-      observer.observe(coversSection);
-    }
-  });
+    });
 }
-//reviews
- export const fetchReviews = async () => {
-    try {
-      const response = await fetch('https://portfolio-js.b.goit.study/api/reviews', {
-        headers: {
-          'Accept': 'application/json'
-        }
-      });
-      if (!response.ok) {
-        if (response.status === 404) {
-          throw new Error('Not found');
-        } else if (response.status === 500) {
-          throw new Error('Server error');
-        } else {
-          throw new Error('An unknown error occurred');
-        }
-      }
-      const reviews = await response.json();
-      return reviews;
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  };
 
-  export const createReviewItem = (review) => {
+//reviews
+export const fetchReviews = async () => {
+    try {
+        const response = await fetch('https://portfolio-js.b.goit.study/api/reviews', {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        if (!response.ok) {
+            if (response.status === 404) {
+                throw new Error('Not found');
+            } else if (response.status === 500) {
+                throw new Error('Server error');
+            } else {
+                throw new Error('An unknown error occurred');
+            }
+        }
+        const reviews = await response.json();
+        return reviews;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+export const createReviewItem = (review) => {
     return `
-      <li class="swiper-slide reviews-items">
-        <img class="reviews-img" src="${review.avatar_url}" alt="${review.author}" />
-        <h3 class="review-author">${review.author}</h3>
-        <p class="review-text">${review.review}</p>
-      </li>
+        <li class="swiper-slide reviews-items">
+            <img class="reviews-img" src="${review.avatar_url}" alt="${review.author}" />
+            <h3 class="review-author">${review.author}</h3>
+            <p class="review-text">${review.review}</p>
+        </li>
     `;
-  };
+};
 
 export const initializeSlider = () => {
-  const swiper = new Swiper('.reviews-swiper', {
-      navigation: {
-      nextEl: '.review-button-next',
-      prevEl: '.review-button-prev',
-    },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 16,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 16,
-      },
-      1440: {
-        slidesPerView: 4,
-        spaceBetween: 16,
-      },
-    },
-    on: {
-      reachEnd: () => {
-        document.querySelector('.review-button-next').classList.add('disabled');
-      },
-      reachBeginning: () => {
-        document.querySelector('.review-button-prev').classList.add('disabled');
-      },
-      fromEdge: () => {
-        document.querySelector('.review-button-prev').classList.remove('disabled');
-        document.querySelector('.review-button-next').classList.remove('disabled');
-      },
-    },
-  });
+    const swiper = new Swiper('.reviews-swiper', {
+        navigation: {
+            nextEl: '.review-button-next',
+            prevEl: '.review-button-prev',
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 16,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 16,
+            },
+            1440: {
+                slidesPerView: 4,
+                spaceBetween: 16,
+            },
+        },
+        on: {
+            reachEnd: () => {
+                document.querySelector('.review-button-next').classList.add('disabled');
+            },
+            reachBeginning: () => {
+                document.querySelector('.review-button-prev').classList.add('disabled');
+            },
+            fromEdge: () => {
+                document.querySelector('.review-button-prev').classList.remove('disabled');
+                document.querySelector('.review-button-next').classList.remove('disabled');
+            },
+        },
+    });
 };
 
 export const displayReviews = async () => {
-  try {
-    const reviews = await fetchReviews(); 
-    const reviewsList = document.querySelector('.reviews-list');
-    reviewsList.innerHTML = reviews.map(createReviewItem).join(''); 
-    initializeSlider();
-  } catch (error) {
-    document.querySelector('.reviews-container').innerHTML = `<p class="error-message">${error.message}</p>`;
-  }
+    try {
+        const reviews = await fetchReviews();
+        const reviewsList = document.querySelector('.reviews-list');
+        reviewsList.innerHTML = reviews.map(createReviewItem).join('');
+        initializeSlider();
+    } catch (error) {
+        document.querySelector('.reviews-container').innerHTML = `<p class="error-message">${error.message}</p>`;
+    }
 };
+
 //worktogether
- export function validateEmail(email) {
-        const emailPattern = /^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-        return emailPattern.test(email);
-};
-    const feedbackForm = document.querySelector('.js-form');
+export function validateEmail(email) {
+    const emailPattern = /^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    return emailPattern.test(email);
+}
+
+const feedbackForm = document.querySelector('.js-form');
 const modalWindow = document.querySelector('.js-modal-backdrop');
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && modalWindow.classList.contains('is-open')) {
-      modalWindow.classList.remove('is-open');
-      document.body.style.overflow = 'auto';
-  }
+    if (e.key === 'Escape' && modalWindow.classList.contains('is-open')) {
+        modalWindow.classList.remove('is-open');
+        document.body.style.overflow = 'auto';
+    }
 });
 
 function eventHandler(e) {
-  e.preventDefault();
-  modalWindow.classList.add('is-open');
-  document.body.style.overflow = 'hidden';
+    e.preventDefault();
+    modalWindow.classList.add('is-open');
+    document.body.style.overflow = 'hidden';
 }
 
 function modalWindowClose(e) {
     const modalClose = e.target.closest('.js-modal-close');
-  if (e.target === modalWindow || modalClose) {
-    modalWindow.classList.remove('is-open');
-    document.body.style.overflow = 'auto';
-  }
+    if (e.target === modalWindow || modalClose) {
+        modalWindow.classList.remove('is-open');
+        document.body.style.overflow = 'auto';
+    }
 }
 
 export { feedbackForm, modalWindow, eventHandler, modalWindowClose };
-//menu
